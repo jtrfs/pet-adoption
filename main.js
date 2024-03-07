945d1c3cf5ec5bfa93eac34ebf931bb1430884d8
Jan Varmuza <jan.varmuza@gmail.com>
2024-03-07 15:23:23 +0100

    Working on the Pets Area, fetched pet data

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
-const temp = document.querySelector('.temp');
-const url = 'https://api.weather.gov/gridpoints/MFL/110,50/forecast';
+const weatherUrl = 'https://api.weather.gov/gridpoints/MFL/110,50/forecast';
+const petsUrl = 'https://learnwebcode.github.io/bootcamp-pet-data/pets.json';
 
+const temp = document.querySelector('.temp');
+
+// fetching the data from the weather api
 async function start() {
-  const weatherDataPromise = await fetch(url);
+  const weatherDataPromise = await fetch(weatherUrl);
   const weatherData = await weatherDataPromise.json();
   console.log('weather data from start():', weatherData);
   const tempNow = weatherData.properties.periods[0].temperature;
   temp.textContent = tempNow + '\u00B0F';
-  console.log(tempNow);
 }
 
 start();
+
+// fetching the data for pets area
+const petsArea = async function () {
+  const petsPromise = await fetch(petsUrl);
+  const petsData = await petsPromise.json();
+  console.log('pets data from petsArea():', petsData);
+  petsData.forEach(pet => {
+    console.log(pet.name);
+  });
+};
+
+petsArea();
